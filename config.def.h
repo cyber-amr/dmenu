e630c6f56e3086737793c512c38ade49ffb3cd72
Amr <amr@programmer.net>
2025-06-15 13:14:38 +0300

    colorize

--- config.def.h
+++ config.def.h
@@ -4,14 +4,14 @@
 static int topbar = 1;                      /* -b  option; if 0, dmenu appears at bottom     */
 /* -fn option overrides fonts[0]; default X11 font or font set */
 static const char *fonts[] = {
-	"monospace:size=10"
+	"fixed:size=12"
 };
 static const char *prompt      = NULL;      /* -p  option; prompt to the left of input field */
 static const char *colors[SchemeLast][2] = {
 	/*     fg         bg       */
-	[SchemeNorm] = { "#bbbbbb", "#222222" },
-	[SchemeSel] = { "#eeeeee", "#005577" },
-	[SchemeOut] = { "#000000", "#00ffff" },
+	[SchemeNorm] = { "#d0d0d0", "#0f0f11" },
+	[SchemeSel] = { "#d0d0d0", "#221144" },
+	[SchemeOut] = { "#1f1f2f", "#1cacfc" },
 };
 /* -l option; if nonzero, dmenu uses vertical list with given number of lines */
 static unsigned int lines      = 0;
